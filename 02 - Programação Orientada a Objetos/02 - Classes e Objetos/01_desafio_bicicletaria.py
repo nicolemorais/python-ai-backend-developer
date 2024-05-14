1fc5716a47084c9cf9a03c82a6dda6ba70082b57
nicolemorais <nicolemoraisatt@gmail.com>
2024-05-14 17:51:36 -0300

    Resolver desafios para melhor fixação de aprendizado

--- 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
+++ 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
@@ -1,30 +1,37 @@
 class Bicicleta:
+    # Atributos
     def __init__(self, cor, modelo, ano, valor):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
 
+    # Métodos
     def buzinar(self):
-        print("Plim plim...")
+        print("Plim Plim..")
 
     def parar(self):
-        print("Parando bicicleta...")
+        print("Parando a bicicleta...")
         print("Bicicleta parada!")
 
     def correr(self):
-        print("Vrummmmm...")
+        print("Peladando rápido..")
+
+    # def __str__(self):
+    #    return f"Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}"
 
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 
-b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+b1 = Bicicleta("Azul", "Caloi", 2024, 1400)
+
 b1.buzinar()
-b1.correr()
 b1.parar()
+b1.correr()
+
 print(b1.cor, b1.modelo, b1.ano, b1.valor)
 
-b2 = Bicicleta("verde", "monark", 2000, 189)
+b2 = Bicicleta("Verde", "Monark", 2000, 400)
+Bicicleta.buzinar(b2)
 print(b2)
-b2.correr()
